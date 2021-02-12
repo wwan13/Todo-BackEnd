e0b967f5dde5b5bf1ad13672365540b46e42c5fc
KimTaewan <wwan13@naver.com>
2021-02-12 21:43:06 +0900

    [taewan] : add set_complete custom action

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from rest_framework.decorators import permission_classes
-from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from .serializer import TodoSerializer
 from .models import Todo
-from todo import serializer
 
 # Create your views here.
 
@@ -14,16 +15,30 @@ class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-    # permission_classes= [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         
         new_todo = Todo()
-        new_todo.author = request.user
+        # current_user = User.objects.get(username = request.user)
+        # new_todo.author = current_user
+        print(request.user)
+        new_todo.author = User.objects.get(pk=1)
         new_todo.contents = request.data["contents"]
         new_todo.date = request.data["date"]
         new_todo.save()
 
         serializer = TodoSerializer(new_todo)
 
-        return HttpResponseRedirect('http://localhost:3000/')
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['patch'])
+    def set_complete(self, request, *args, **kwargs):
+
+        todo_object = Todo.objects.get(pk = request.id)
+        todo_object.state = 'complete'
+        todo_object.save()
+
+        serializer = TodoSerializer(todo_object)
+
+        return Response(serializer.data)
