63722c702e1598a0f93030e9d879bfc9e7a1d5d8
KimTaewan <wwan13@naver.com>
2021-02-14 23:10:46 +0900

    [taewan] : add filtering method

--- todo/views.py
+++ todo/views.py
@@ -12,18 +12,36 @@ from .models import Todo
 # Create your views here.
 
 class TodoViewSet(ModelViewSet):
-    queryset = Todo.objects.all()
+    # queryset = Todo.objects.all()
+    queryset = Todo.objects.order_by('-state', '-timeline')
     serializer_class = TodoSerializer
 
     # permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+
+        print(self.request.query_params.get('filter', ''))
+
+        filter_keyword = self.request.query_params.get('filter', None)
+
+        if filter_keyword == 'ongoing':
+            queryset = Todo.objects.filter(state = 'ongoing')
+        elif filter_keyword == 'complete':
+            queryset = Todo.objects.filter(state = 'ongoing')
+        else:
+            queryset = Todo.objects.filter(state = 'ongoing')
+        
+        return queryset
+
+
     def create(self, request, *args, **kwargs):
         
         new_todo = Todo()
-        # current_user = User.objects.get(username = request.user)
+        # current_user = User.objects.get(username = request.user.name)
         # new_todo.author = current_user
-        print(request.user)
-        new_todo.author = User.objects.get(pk=1)
+        print(request.user.__dict__)
+        new_todo.author = User.objects.get(pk = 1)
+
         new_todo.contents = request.data["contents"]
         new_todo.date = request.data["date"]
         new_todo.save()
@@ -32,11 +50,22 @@ class TodoViewSet(ModelViewSet):
 
         return Response(serializer.data)
 
-    @action(detail=True)
+    @action(detail = True)
     def set_complete(self, request, *args, **kwargs):
 
         todo_object = self.get_object()
         todo_object.state = 'complete'
+        todo_object.save()
+
+        serializer = TodoSerializer(todo_object)
+
+        return Response(serializer.data)
+
+    @action(detail = True)
+    def set_ongoing(self, request, *args, **kwargs):
+
+        todo_object = self.get_object()
+        todo_object.state = 'ongoing'
         todo_object.save()
 
         serializer = TodoSerializer(todo_object)
