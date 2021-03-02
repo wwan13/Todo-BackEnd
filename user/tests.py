97f0ea403d6f956b13a8f0400022849c8197ca01
KimTaewan <wwan13@naver.com>
2021-03-02 22:02:45 +0900

    [taewan] : add user login unit test

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
-
+from django.http import response
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+import json
 # Create your tests here.
+
+class UserTest(TestCase):
+
+    client = Client()
+
+    def setUp(self):
+        user = User.objects.create(
+            username = "test123",
+            password = "testpassword1020",
+        )
+
+    def test_login_success(self):
+
+        data = {
+            "username" : "test123",
+            "password" : "testpassword1020",
+        }
+
+        response = self.client.post("/rest-auth/login/", json.dumps(data), content_type="application/json")
+
+        self.assertEqual(response.status_code,200)
