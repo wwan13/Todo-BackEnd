872a3ca5a4d2ef245dd8fb19900feaf422becc5a
KimTaewan <wwan13@naver.com>
2021-03-03 09:50:50 +0900

    [taewan] : add todo unit test

--- todo/tests.py
+++ todo/tests.py
@@ -1,8 +1,10 @@
+from django.http import response
 from django.test import TestCase, Client
 from .models import Todo
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.forms.models import model_to_dict
+from django.core.serializers.json import DjangoJSONEncoder
 import json
 
 # Create your tests here.
@@ -41,15 +43,15 @@ class TodoTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-    def test_new_todo_object_success(self):
-        user = User.objects.all()[0]
-        todo = Todo.objects.create(
-            date = "2020-12-01",
-            contents = "aassdd",
-            author = user
-        )
-        todo_data = model_to_dict(todo)
-        
-        response = self.client.post(self.URL, json.dumps(todo_data), content_typt = "application/json")
+    def test_make_todo_success(self):
+
+        header = {"Authorization" : "f6b143bda885e4fbd8c90227a879eda162c3dadb",} 
+
+        data = {
+            "date" : "2020-10-21",
+            "contents" : "unit test",
+        }
+
+        response = self.client.post(self.URL, json.dumps(data), content_type="application/json", **header)
 
         self.assertEqual(response.status_code, 200)
